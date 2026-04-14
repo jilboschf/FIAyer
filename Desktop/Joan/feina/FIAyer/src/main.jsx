26b258a28155f63acd60f69be9e6580a7d7a5918
Joan <jilbosch@icloud.com>
2026-04-14 15:50:25 +0200

    Moure fitxers a /api i preparar versió final

--- Desktop/Joan/feina/FIAyer/src/main.jsx
+++ Desktop/Joan/feina/FIAyer/src/main.jsx
@@ -1,3 +1,17 @@
+// AFEGEIX AQUESTA IMPORTACIÓ A DALT DE TOT
+import { useTranslation } from 'react-i18next';
+
+function Header() {
+  // AQUESTA LÍNIA ÉS LA QUE T'ESTÀ FALTANT:
+  const { t, i18n } = useTranslation(); 
+
+  // Ara ja pots fer servir "i18n.changeLanguage" sense que peti
+  return (
+    <button onClick={() => i18n.changeLanguage('ca')}>
+      {t('header.lang')}
+    </button>
+  );
+}
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
